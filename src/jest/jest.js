b85a6e72970f4447095eb3da87dfee0a0c9e2568
zy410419243 <zy410419243@gmail.com>
2019-01-12 20:06:09 +0800

    feat: load mock files from cli not target repo

--- src/jest/jest.js
+++ src/jest/jest.js
@@ -14,8 +14,10 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testURL: 'http://localhost',
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.join(
+      __dirname,
+      '../../__mocks__/fileMock.js',
+    ),
+    '\\.(css|scss)$': path.join(__dirname, '../../__mocks__/styleMock.js'),
   },
 };
