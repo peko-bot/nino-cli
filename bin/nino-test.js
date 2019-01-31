7244c8f19b10f5ec661e3163fbcae2c62c5b07dd
zy410419243 <zy410419243@gmail.com>
2019-01-31 14:57:01 +0800

    pref: add coverage using jest

--- bin/nino-test.js
+++ bin/nino-test.js
@@ -3,6 +3,9 @@
 const program = require('commander');
 const nino = require('../src/test-jest');
 
-program.option('-u, --update', 'to update snapshots').parse(process.argv);
+program
+  .option('-u, --update', 'to update snapshots')
+  .option('-d, --codecov', 'coverage using jest')
+  .parse(process.argv);
 
 nino.test(program);
--- src/test-jest.js
+++ src/test-jest.js
@@ -27,11 +27,19 @@ function runCmd(cmd, args, callback) {
 exports.test = program => {
   // jest --config jest.js --verbose=false
   const isUpdate = !!program.update;
+  const codecov = !!program.codecov;
   const jestBin = require.resolve('jest/bin/jest');
   const jestConfig = path.join(__dirname, '../src/jest/jest.js');
   let args = [jestBin, '--config', jestConfig];
   if (isUpdate) {
     args.push('-u');
+  }
+  if (codecov) {
+    args.push('--verbose');
+    args.push('false');
+    args.push('-w');
+    args.push('1');
+    args.push('--coverage');
   }
   runCmd('node', args);
 };
