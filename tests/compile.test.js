27cc5a238044896993398c06c8b49476518ef6ac
orzyyyy <zy410419243@gmail.com>
2019-07-09 17:02:50 +0800

    test: update cases for compile

--- tests/compile.test.js
+++ tests/compile.test.js
@@ -11,7 +11,7 @@ describe('nino compile', () => {
 
   getContent = paths => fs.readFileSync(joinWithRootPath(paths)).toString();
 
-  it('default', () => {
+  it('compile correctly', () => {
     const libContent = getContent(['lib', 'default.js']);
     expect(libContent).toBe(`"use strict";
 
@@ -20,6 +20,15 @@ var _dateFns = require("date-fns");
 var defaultFunc = function defaultFunc(prefix) {
   // tslint:disable-next-line: no-console
   console.log("".concat(prefix, ": ").concat((0, _dateFns.format)(new Date(), 'yyyy-MM-dd hh:mm:ss')));
+};
+
+defaultFunc('timeStamp:');`);
+    const esContent = getContent(['es', 'default.js']);
+    expect(esContent).toBe(`import { format } from 'date-fns';
+
+var defaultFunc = function defaultFunc(prefix) {
+  // tslint:disable-next-line: no-console
+  console.log("".concat(prefix, ": ").concat(format(new Date(), 'yyyy-MM-dd hh:mm:ss')));
 };
 
 defaultFunc('timeStamp:');`);
