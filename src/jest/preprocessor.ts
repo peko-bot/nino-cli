24d41c0a15304649f04902b1130f703d4f4128df
orzyyyy <zy410419243@gmail.com>
2019-07-11 06:19:19 +0800

    fix: test problem with jsx

--- src/jest/preprocessor.ts
+++ src/jest/preprocessor.ts
@@ -1,20 +1,34 @@
-import { createTransformer } from 'ts-jest';
-import fs from 'fs-extra';
+const { createTransformer: babelTransFormer } = require('babel-jest');
+const { createTransformer: tsTransFormer } = require('ts-jest');
+import { getBabelConfig } from '../babel/babelCommonConfig';
 import { joinWithRootPath } from '../utils/common';
+import fs from 'fs-extra';
 
-const testTsConfigPath = joinWithRootPath('tsconfig.test.json');
-const tsTestConfigPath = fs.existsSync(testTsConfigPath)
-  ? testTsConfigPath
+const tsTestConfigPath = fs.existsSync(joinWithRootPath('tsconfig.test.json'))
+  ? joinWithRootPath('tsconfig.test.json')
   : joinWithRootPath('tsconfig.json');
-const tsJest = createTransformer({
+const tsJest = tsTransFormer({
   tsConfig: tsTestConfigPath,
 });
+const babelConfig = getBabelConfig();
+babelConfig.plugins = [...babelConfig.plugins];
+const babelJest = babelTransFormer(babelConfig);
 
 module.exports = {
   process(src: any, filePath: string) {
-    src = tsJest.process(src, filePath, {
-      moduleFileExtensions: ['ts', 'tsx'],
-    } as any);
+    const isTypeScript = filePath.endsWith('.ts') || filePath.endsWith('.tsx');
+    const isJavaScript = filePath.endsWith('.js') || filePath.endsWith('.jsx');
+    if (isTypeScript) {
+      src = tsJest.process(src, filePath, {
+        moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+      });
+    } else if (isJavaScript) {
+      src = babelJest.process(src, filePath, {
+        moduleFileExtensions: ['js', 'jsx'],
+      });
+    } else {
+      throw new Error(`File not match type: ${filePath}`);
+    }
     return src;
   },
 };
