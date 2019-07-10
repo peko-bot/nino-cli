295b8ffedcb916ec436ff2eca9f7a05e1edaa21e
orzyyyy <zy410419243@gmail.com>
2019-07-10 20:38:26 +0800

    perf: redesign test of cli (#28)

--- src/jest/index.ts
+++ src/jest/index.ts
@@ -1,26 +1,21 @@
 import path from 'path';
 import getRemainingArgs from 'commander-remaining-args';
-import { runCmd } from './utils/common';
+import { runCmd } from '../utils/common';
 
 export const test = (program: any) => {
-  const isUpdate = !!program.update;
-  const codecov = !!program.codecov;
-  const watch = !!program.watch;
+  const { update, codecov, watch } = program;
   const jestBin = require.resolve('jest/bin/jest');
-  const jestConfig = path.join(__dirname, './jest/jest.js');
+  const jestConfig = path.join(__dirname, './jest.js');
   let args = [jestBin, '--config', jestConfig];
-  if (isUpdate) {
+  if (update) {
     args.push('-u');
   }
   if (codecov) {
-    args.push('-w');
-    args.push('1');
     args.push('--coverage');
   }
   if (watch) {
     args.push('--watch');
   }
-
   const remainingArgs = getRemainingArgs(program);
   args = [...args, ...remainingArgs];
   runCmd('node', args);
