06eed60a22fe85baf72a30e87e3d6f42d935b6d6
orzyyyy <zy410419243@gmail.com>
2019-08-01 20:20:17 +0800

    feat: support eslint for typescript (#39)
    * feat: support eslint for typescript
    
    * fix: update command for lint

---
+++ src/entry/nino-eslint.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+import { eslint } from '../eslint';
+
+program
+  .option(
+    '-c, --lintConfigPath <path>',
+    'set config path, such as .eslintrc.js',
+  )
+  .option(
+    '-i, --ignoreConfigPath <path>, such as .eslintignore',
+    'files you want to ignore with eslint',
+  )
+  .option(
+    '--checkPaths [path]',
+    'files need check, such as src/**',
+    '**/*.ts[x]',
+  )
+  .allowUnknownOption()
+  .parse(process.argv);
+
+eslint(program);
