f0e53ee4df5e67291f8ee37f105012de2d55a0bd
orzyyyy <zy410419243@gmail.com>
2019-10-22 09:14:11 +0800

    chore: alias of testPathPattern for test

--- src/entry/nino-test.ts
+++ src/entry/nino-test.ts
@@ -7,6 +7,7 @@ program
   .option('-u, --update', 'to update snapshots', false)
   .option('--codecov', 'coverage using jest', false)
   .option('-w, --watch', 'watch mode', false)
+  .option('-p, --path <path>', 'alias of testPathPattern')
   .allowUnknownOption()
   .parse(process.argv);
 
