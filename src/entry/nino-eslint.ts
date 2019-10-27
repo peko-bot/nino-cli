79886c70a5b48e3dc0af652532e6f83c6be24c11
orzyyyy <zy410419243@gmail.com>
2019-10-27 10:54:33 +0800

    fix(eslint): default checkPaths can't match *.ts

--- src/entry/nino-eslint.ts
+++ src/entry/nino-eslint.ts
@@ -6,7 +6,7 @@ import { eslint } from '../eslint';
 program
   .option('-c, --lintConfigPath <path>', 'set config path, such as .eslintrc.js')
   .option('-i, --ignoreConfigPath <path>, such as .eslintignore', 'files you want to ignore with eslint')
-  .option('--checkPaths [path]', 'files need check, such as src/**', '**/*.ts[x]')
+  .option('--checkPaths [path]', 'files need check, such as src/**/*.ts', '**/*.@(ts|tsx)')
   .allowUnknownOption()
   .parse(process.argv);
 
