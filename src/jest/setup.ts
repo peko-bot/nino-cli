5248844deaa46ff172116b1680937afd9d3225be
orzyyyy <zy410419243@gmail.com>
2021-11-18 20:44:10 +0800

    ditto

--- src/jest/setup.ts
+++ src/jest/setup.ts
@@ -1,7 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import * as Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 const g = global as any;
 g.requestAnimationFrame =
@@ -10,4 +10,4 @@ g.requestAnimationFrame =
     setTimeout(callback, 0);
   };
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new (Adapter as any)() });
